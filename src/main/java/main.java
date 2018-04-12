871f0960e8666d9039ee5f837fc0a9cf63dd3f5a
killking <472352334@qq.com>
2018-04-12 19:28:05 +0800

    克隆 测设

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,5 +5,6 @@ public class main {
     public static void main (String []args){
             System.out.println("hello, baby!!!");
             System.out.println("vao vao");
+            System.out.println("克隆记载");
     }
 }
